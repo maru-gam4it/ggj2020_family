93186ff3d73af5f52ce4e16141ef0960b096fad9
Ilan González Carigi <locoilangonzalez@yahoo.com.mx>
2020-02-02 11:46:44 +0900

    Unity

---
+++ Assets/Scenes/Scripts/ClickableObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ClickableObject : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Target object that changes sprite")]
+    GameObject targetObject = null;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown() {
+        if (Input.GetMouseButtonDown(0)) {
+            if (targetObject != null) {
+                if (targetObject.TryGetComponent(out EventObject targetScrpit)) {
+                    targetScrpit.ChangeSprite();
+                    this.gameObject.SetActive(false);
+                } else {
+#if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                    UnityEditor.EditorUtility.DisplayDialog("Error", targetObject.name + " doesnt have EventObject scprit attached", "Ok");
+#endif
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/EventObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventObject : MonoBehaviour
+{
+    [SerializeField]
+    Sprite secondSprite = null;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeSprite() {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = secondSprite;
+    }
+}
